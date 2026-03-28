7221e15b07ba8c386baa97b0d2568422d84a39c6
Furkan SARICA <sarica.furkan@icloud.com>
2026-03-28 16:23:39 +0300

    Add VPC variables to variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,46 @@
+variable "name" {
+  description = "Name prefix for all resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "az_count" {
+  description = "Number of availability zones to use"
+  type        = number
+  default     = 3
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway for private subnets"
+  type        = bool
+  default     = true
+}
+
+variable "single_nat_gateway" {
+  description = "Use a single NAT Gateway (cost savings for non-prod)"
+  type        = bool
+  default     = false
+}
+
+variable "enable_flow_logs" {
+  description = "Enable VPC Flow Logs"
+  type        = bool
+  default     = true
+}
+
+variable "flow_logs_retention" {
+  description = "CloudWatch log group retention in days"
+  type        = number
+  default     = 30
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
